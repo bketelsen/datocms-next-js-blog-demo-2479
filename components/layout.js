83723e9edf6969b6688c29858526f3f1055a1ec7
Brian Ketelsen <mail@bjk.fyi>
2021-01-02 09:28:11 -0500

    ghostly

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,6 @@ import Alert from '../components/alert'
 export default function Layout({ preview, children }) {
   return (
     <>
-      <Alert preview={preview} />
       <main>{children}</main>
     </>
   )
